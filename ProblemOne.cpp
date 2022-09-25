c98742ab54bb03834044b567945460fae06b242f
saksham sharma <sakshamnsit66@gmail.com>
2022-09-25 13:42:25 +0530

    Create ProblemOne.cpp
    https://leetcode.com/contest/weekly-contest-312/problems/sort-the-people/

---
+++ ProblemOne.cpp
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/contest/weekly-contest-312/problems/sort-the-people/
+
+2418. Sort the People
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+Return names sorted in descending order by the people's heights.
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+ 
+
+Constraints:
+
+    n == names.length == heights.length
+    1 <= n <= 103
+    1 <= names[i].length <= 20
+    1 <= heights[i] <= 105
+    names[i] consists of lower and upper case English letters.
+    All the values of heights are distinct.
+
+*/
+
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int,string>> v;
+        for(int i=0;i<names.size();i++) 
+            v.push_back({heights[i],names[i]});
+        
+        sort(v.begin(),v.end());
+        vector<string> tmp; 
+        for(int i=0;i<names.size();i++) tmp.push_back(v[i].second);
+        reverse(tmp.begin(),tmp.end());
+        return tmp;
+    }
+};
